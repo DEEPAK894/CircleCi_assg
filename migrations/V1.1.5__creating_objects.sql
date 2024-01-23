472fb76f687b19d107af5e75f8d68777426d438e
Sigmoid <sigmoid@root@sigmoidanalytics.com>
2024-01-23 14:53:03 +0530

    FINAL COMMIT

--- migrations/V1.1.5__creating_objects.sql
+++
@@ -1,7 +0,0 @@
-USE SCHEMA DEMO;
--- Original script: 001_create_table_users.sql
-CREATE TABLE users (
-    id INT,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
--- migrations/V1.1.6__created_objects.sql
+++
@@ -1,6 +0,0 @@
-USE SCHEMA DEMO;
--- Original script: 001_create_table_users.sql
-CREATE TABLE users (
-    id INT,
-    name VARCHAR(255)
-);
