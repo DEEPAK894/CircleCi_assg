5112882d6ed7d1d657787b6f1e7c7462c3b92aee
Sigmoid <sigmoid@root@sigmoidanalytics.com>
2024-01-23 12:35:36 +0530

    initial commit

---
+++ migrations/V1.1.4__updated_objects.sql
@@ -0,0 +1,10 @@
+USE SCHEMA DEMO;
+-- Original script: 001_create_table_users.sql
+CREATE TABLE users (
+    id INT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+-- Rollback script: 001_rollback_create_table_users.sql
+DROP TABLE IF EXISTS users;
