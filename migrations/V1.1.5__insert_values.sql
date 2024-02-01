e051dae5101a33baaaef47e25374055ca3630550
Sigmoid <sigmoid@root@sigmoidanalytics.com>
2024-02-01 16:03:22 +0530

    initial commit

--- migrations/V1.1.5__insert_values.sql
+++ migrations/V1.1.5__insert_values.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 
 -- Your DELETE statement
 DELETE FROM SF_CICD.TEMP_SCHEMA.PRODUCT
-WHERE ProductID = 202;
+WHERE ProductID = 203;
 
 -- If something goes wrong, you can roll back the transaction
 ROLLBACK;
