1e819ceb23141dd34c860b6f48dd1e6c33e45723
Sigmoid <sigmoid@root@sigmoidanalytics.com>
2024-02-01 15:39:14 +0530

    initial commit

---
+++ migrations/V1.1.1__insert_values.sql
@@ -0,0 +1,16 @@
+-- Use the INFORMATION_SCHEMA to get the current session's temporary schema
+SELECT CURRENT_SCHEME() AS temp_schema;
+
+-- Create a temporary table within the current session's schema
+CREATE TEMPORARY TABLE temp_schema.PRODUCT (
+  -- Define your table columns here based on your CSV file
+  ID INT,
+  Name STRING,
+  -- Add more columns as needed
+);
+
+-- Copy data into the temporary table
+COPY INTO temp_schema.PRODUCT
+FROM SATGE_NEW/dataset-Sheet1.csv
+FILE_FORMAT = (FORMAT_NAME = my_csv_format)
+ON_ERROR = 'CONTINUE';
