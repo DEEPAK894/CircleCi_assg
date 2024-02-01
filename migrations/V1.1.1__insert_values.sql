fec262bd9cde3949b2b6f75093447b16dec9066b
Sigmoid <sigmoid@root@sigmoidanalytics.com>
2024-02-01 15:53:01 +0530

    initial commit

--- migrations/V1.1.1__insert_values.sql
+++ migrations/V1.1.1__insert_values.sql
@@ -1,12 +1,4 @@
--- Use the INFORMATION_SCHEMA to get the current session's temporary schema
-CREATE SCHEMA temp_schema;
-
--- Create a temporary table within the current session's schema
-CREATE TEMPORARY TABLE temp_schema.PRODUCT ;
-
-
--- Copy data into the temporary table
-COPY INTO temp_schema.PRODUCT
-FROM SATGE_NEW/dataset-Sheet1.csv
-FILE_FORMAT = (FORMAT_NAME = my_csv_format)
-ON_ERROR = 'CONTINUE';
+UPDATE  SF_CICD.TEMP_SCHEMA.PRODUCT 
+SET Price = 119.99
+WHERE ProductID = 202;
+ROLLBACK
